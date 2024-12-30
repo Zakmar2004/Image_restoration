cb188e3d9715e787963714683fbe4fa2d5a4fae0
UllubiyZabitov <unzabitov@edu.hse.ru>
2024-12-30 20:39:17 +0300

    Add files via upload

--- gfpgan_inference.py
+++ gfpgan_inference.py
@@ -5,7 +5,6 @@ import os
 
 def enhance_with_gfpgan(input_image_path, upscale=2):
     with tempfile.TemporaryDirectory() as temp_dir:
-
         command = [
             "python", "GFPGAN/inference_gfpgan.py",
             "-i", input_image_path,
@@ -14,14 +13,18 @@ def enhance_with_gfpgan(input_image_path, upscale=2):
             "-s", str(upscale),
             "--bg_upsampler", "realesrgan"
         ]
-
-        subprocess.run(command, check=True)
+        try:
+            subprocess.run(command, check=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e.stderr.decode()}")
+            return None
 
         restored_img_dir = os.path.join(temp_dir, 'restored_imgs')
         result_image_path = os.path.join(restored_img_dir, 'input_image.jpg')
         enhanced_image = Image.open(result_image_path)
 
         return enhanced_image
+
 
 
 
